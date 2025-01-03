5754d7bf2b28af512b07994694b24c977a0cf8e8
Frank Delporte <frank@webtechie.be>
2025-01-03 15:12:39 +0100

    Cleanup

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-scp target/picam-controller.jar pi@af-cam-3.local://home/pi/picam-controller/picam-controller.jar
+scp target/picam-controller.jar pi@af-cam-4.local://home/pi/picam-controller/picam-controller.jar
